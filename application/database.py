86dd17559029a5659838340fa18cf28336cffef5
a-pranathi <123293149+a-pranathi@users.noreply.github.com>
2026-03-30 15:54:12 +0530

    Update database.py

--- application/database.py
+++ application/database.py
@@ -1,8 +1,6 @@
 """
 File:    database.py
-Author1: Pranathi Ayyadevara
-Roll No: 22f3001788
-Course:  MAD 1 Project - LMS
+Author: Pranathi Ayyadevara
 Summary of File:
 	This file contains code which creates the database connection
 """
